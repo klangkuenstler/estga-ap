589877b37b39b1b493d70fbe5aa3bf08abaf5b86
Marcos Ribeiro <marcospribeiro57@gmail.com>
2025-10-01 21:10:36 +0100

    class 5 and 6

---
+++ exercises/ex1/ex1.py
@@ -0,0 +1,8 @@
+# 2025-10-01
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+
+num3 = num1 + num2
+
+print(f"The sum of {num1} and {num2} is {num3}")
---
+++ exercises/ex2/ex2.py
@@ -0,0 +1,11 @@
+# 2025-10-01
+# '03_introducao_python.pdf'
+'''
+27
+EXERCÍCIO
+▪ Desenvolva, usando pseudo-código, um programa que:
+    ▪ indique qual o dia da semana se for introduzido um número de 1 a 7 (primeiro dia da semana é domingo)
+    ▪ caso seja introduzido um número fora do intervalo, deve ser enviada a mensagem “Dia da semana (<número_introduzido>) inválido!”
+    ▪ Implemente o algoritmo usando a linguagem de programação Python e teste o seu correcto funcionamento
+'''
+
