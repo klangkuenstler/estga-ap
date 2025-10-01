d6882868de4a09e9ece2bf8464a8bacf9d5e4636
Marcos Ribeiro <marcospribeiro57@gmail.com>
2025-10-01 21:55:05 +0100

    class 6

--- exercises/ex2/ex2.py
+++ exercises/ex2/ex2.py
@@ -1,5 +1,5 @@
 # 2025-10-01
-# '03_introducao_python.pdf' slide n27
+# '03_introducao_python.pdf' slide n27 (Segunda parte - desenvolver o programa em linguagen python)
 '''
 EXERCÍCIO
 ▪ Desenvolva, usando pseudo-código, um programa que:
@@ -7,6 +7,28 @@ EXERCÍCIO
     ▪ caso seja introduzido um número fora do intervalo, deve ser enviada a mensagem “Dia da semana (<número_introduzido>) inválido!”
     ▪ Implemente o algoritmo usando a linguagem de programação Python e teste o seu correcto funcionamento
 '''
+
+def dia_da_semana(numero):
+    if numero == 1:
+        return "Domingo"
+    elif numero == 2:
+        return "Segunda-feira"
+    elif numero == 3:
+        return "Terça-feira"
+    elif numero == 4:
+        return "Quarta-feira"
+    elif numero == 5:
+        return "Quinta-feira"
+    elif numero == 6:
+        return "Sexta-feira"
+    elif numero == 7:
+        return "Sábado"
+    else:
+        return f"Dia da semana ({numero}) inválido!"
+        exit()
+
+user_input
+print
 
 
 
