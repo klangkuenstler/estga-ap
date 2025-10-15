93909afd7327201eaddc935916442698f52411fd
Marcos Ribeiro <marcospribeiro57@gmail.com>
2025-10-15 20:01:28 +0100

    bakcup

--- exercises/gp2/ex4mult3.py
+++ exercises/gp2/ex4mult3.py
@@ -1,11 +1,14 @@
 m = int(input("Valor de m: "))
 M = int(input("Valor de M: "))
 
-if m > M:
+# Validação
+if m >= M:
     print("'m' deve ser menor que 'M'.")
-
-
-m_string = f"{m}"
-M_string = f"{M}"
-
-print(f"m = " + m_string + "\nM = " + M_string)
+else:
+    print(f"m = {m}")
+    print(f"M = {M}")
+    
+    # Imprimir múltiplos de 3 entre m e M
+    for num in range(m, M + 1):
+        if num % 3 == 0:
+            print(num)
